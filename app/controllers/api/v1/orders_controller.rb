b99c579572ff2c61968679b858a8803688a442fc
Craft Academy <craftacademy@Crafts-MacBook-Air.local>
2020-03-07 18:28:52 +0100

    adds order and order_item models

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::OrdersController < ApplicationController
   def create
     order = Order.create
-    order.order_items.create(product:id: params[:product_id])
-    render json: { message: 'The product has been added to your order' order_id: order_id }
+    order.order_items.create(product_id: params[:product_id])
+    render json: { message: 'The product has been added to your order', order_id: order_id }
   end
 end
