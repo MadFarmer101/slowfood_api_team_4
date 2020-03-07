91431d5986e74ee727870bb38ce8e01a10639277
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-07 18:41:53 +0100

    added demo and controllers / models order

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+products = Product.create(category: "burgers", name: "Cheeseburger", description: "Bun, meat and cheese", price: 100 )
+products = Product.create(category: "pizza", name: "Vesuvio", description: "By Janko with love", price: 70 )
+users = User.create( email: "user@mail.com", password: "password", password_confirmation: "password" )
