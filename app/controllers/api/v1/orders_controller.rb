ffbe954a525a8f01186caf4e772410ca2aaeb7f1
Janko <mailodjanka@gmail.com>
2020-03-08 13:30:38 +0100

    Trying to added name of the meal to order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,15 +1,21 @@
 class Api::V1::OrdersController < ApplicationController
-
   def create
     order = Order.create
     order.order_items.create(product_id: params[:product_id])
-    render json: { message: 'The product has been added to your order', order_id: order.id }
+    render json: create_json_response(order)
   end
 
   def update
     order = Order.find(params[:id])
     product = Product.find(params[:product_id])
     order.order_items.create(product: product)
-    render json: { message: 'The product has been added to your order', order_id: order.id }
+    render json: create_json_response(order)
+  end
+
+  private
+
+  def create_json_response(order)
+    json = {}
+    json[:message] = 'The product has been added to your order'
   end
 end
